d269adc92e5b02782c27f7fef9695de03ca067c1
Wayne <499j3018@stust.edu.tw>
2018-05-17 00:52:42 +0800

    測試angular ng-include

---
+++ js/controllers/main.js
@@ -0,0 +1,10 @@
+/**
+ * create by Wayne 2018/05
+ */
+
+    'use strict';
+    var app = angular.module('myApp', []);
+
+    app.controller('MainCtrl', function ($scope) {
+    	$scope.ABC = 123;
+    });
