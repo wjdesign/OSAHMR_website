03ecd588fd6a9b0a0f5d3ff2eabd44904431008b
Wayne <499j3018@stust.edu.tw>
2018-05-17 01:26:42 +0800

    0516 mainPage套用angularJS改寫

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -6,5 +6,62 @@
     var app = angular.module('myApp', []);
 
     app.controller('MainCtrl', function ($scope) {
-    	$scope.ABC = 123;
+
+    	// 假資料區
+    	$scope.MsgMarquee = "本站目前正在維護中，造成您的不便請見諒。";
+    	$scope.News = [
+    		{
+    			'title':'【功能新增與調整】職業安全衛生管理報備資訊網  主要功能新增與調整通知。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'有關勞工健康保護規則第13條第3項「雇主對於健康管理為第三級管理以上之勞工所實施健康追蹤檢查之分級結果及採行措施之通報」資料之「健康追蹤檢查日期」欄位填寫注意事項。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【功能異動】職業安全衛生管理報備資訊網 事業單位帳號管理作業異動通知。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'「事業單位申請帳號授權書」(105.3.29二版) 填寫常見問題與說明，請詳閱附件檔。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【再次提醒】事業單位線上填報從事勞工健康服務醫師或護理人員資料送審後，須另以紙本公文〈 主旨載明備查項目及系統案件編號〉函知所在地勞動檢查機構，方完成備查程序。附件：備查......。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【「事業單位申請帳號」線上填寫常見問題與說明。附件：帳號申請填寫注意事項.pdf。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		}
+    	];
+    	$scope.Cases = [
+    		{
+    			'title':'【功能新增與調整】職業安全衛生管理報備資訊網  主要功能新增與調整通知。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'有關勞工健康保護規則第13條第3項「雇主對於健康管理為第三級管理以上之勞工所實施健康追蹤檢查之分級結果及採行措施之通報」資料之「健康追蹤檢查日期」欄位填寫注意事項。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【功能異動】職業安全衛生管理報備資訊網 事業單位帳號管理作業異動通知。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'「事業單位申請帳號授權書」(105.3.29二版) 填寫常見問題與說明，請詳閱附件檔。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【再次提醒】事業單位線上填報從事勞工健康服務醫師或護理人員資料送審後，須另以紙本公文〈 主旨載明備查項目及系統案件編號〉函知所在地勞動檢查機構，方完成備查程序。附件：備查......。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		},{
+    			'title':'【「事業單位申請帳號」線上填寫常見問題與說明。附件：帳號申請填寫注意事項.pdf。',
+    			'time':'107/05/01',
+    			'content':'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    		}
+    	];
+
     });
